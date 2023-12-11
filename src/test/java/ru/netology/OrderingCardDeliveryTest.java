078416764a637700cdbb63ab3d8c67f0eb557ee6
Ekaterina <shi.e.m@yandex.ru>
2023-12-11 11:12:25 +0300

    second meeting

--- src/test/java/ru/netology/OrderingCardDeliveryTest.java
+++ src/test/java/ru/netology/OrderingCardDeliveryTest.java
@@ -9,8 +9,8 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class OrderingCardDeliveryTest {
 
@@ -24,6 +24,26 @@ public class OrderingCardDeliveryTest {
         $("[data-test-id='city'] input").setValue("Нижний Новгород");
         String planningDate = generateDate(3, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Ирина");
+        $("[data-test-id='phone'] input").setValue("+77654568775");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
+    }
+
+    @Test
+    void successfulCompletionOfTheCardDeliveryOrderFormInteractionWithComplexElements() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Ни");
+        $(withText("Нижний Новгород")).shouldBe(visible).click();
+        $(".icon-button").click();
+        $(".calendar").shouldBe((visible));
+        String planningDate = generateDate(6, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $(".calendar__day").shouldBe(visible).click();
         $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Ирина");
         $("[data-test-id='phone'] input").setValue("+77654568775");
