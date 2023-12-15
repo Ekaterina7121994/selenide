d7c7b9b62c9f2e0b3aa2e4262ce25b54937286f8
Ekaterina <shi.e.m@yandex.ru>
2023-12-15 09:15:49 +0300

    edited second task

--- src/test/java/ru/netology/OrderingCardDeliveryTest.java
+++ src/test/java/ru/netology/OrderingCardDeliveryTest.java
@@ -6,8 +6,11 @@ import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
@@ -38,19 +41,24 @@ public class OrderingCardDeliveryTest {
     void successfulCompletionOfTheCardDeliveryOrderFormInteractionWithComplexElements() {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Ни");
-        $(withText("Нижний Новгород")).shouldBe(visible).click();
-        $(".icon-button").click();
-        $(".calendar").shouldBe((visible));
-        String planningDate = generateDate(6, "dd.MM.yyyy");
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $(".calendar__day").shouldBe(visible).click();
-        $("[data-test-id='date'] input").setValue(planningDate);
+        $$(".menu-item").findBy(text("Нижний Новгород")).click();
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.chord(Keys.BACK_SPACE));
+        LocalDateTime startDate = LocalDateTime.now();
+        LocalDateTime choiceDate = startDate.plusDays(6);
+        int choiceDay = choiceDate.getDayOfMonth();
+        int maxDayMonth = Calendar.getInstance().getActualMaximum(Calendar.DAY_OF_MONTH);
+        if (maxDayMonth - choiceDay < 7) {
+            $(".popup_target_anchor").find(".calendar__arrow_direction_right", 1).click();
+        }
+        String date = choiceDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $(".calendar__layout").$$(".calendar__day").findBy(text(String.valueOf(choiceDay))).click();
         $("[data-test-id='name'] input").setValue("Ирина");
         $("[data-test-id='phone'] input").setValue("+77654568775");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
-        $(".notification__content")
-                .shouldBe(visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
+        $("[data-test-id='notification']")
+                .shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__title").shouldHave(Condition.exactText("Успешно!"));
+        $(".notification__content").shouldHave(Condition.exactText("Встреча успешно забронирована на " + date));
     }
 }
